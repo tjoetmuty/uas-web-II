82a233fd086988d8d7723b8f011cdad1498816ac
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2024-05-22 22:57:52 +0800

    make controller for myClass

--- app/Models/myClass.php
+++ app/Models/myClass.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class myClass extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'mata_kuliah',
+        'nama_dosen',
+        'nama_kelas',
+        'hari',
+        'waktu_mulai',
+        'waktu_selesai',
+        'semester',
+        'prodi',
+        'jurusan',
+        'tahun_akademik',
+    ];
 }
