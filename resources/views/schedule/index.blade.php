f1a7ddc87d7a19a85e7a91bedbeca357d762220f
Tjoet Muty Ahmad <tjmutyahmd27@gmail.com>
2024-05-25 22:22:29 +0800

    handle create data

--- resources/views/schedule/index.blade.php
+++ resources/views/schedule/index.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="https://cdn.tailwindcss.com"></script>
+    @vite('resources/css/app.css')
     <title>Schedule</title>
 </head>
 <body>
@@ -12,14 +12,78 @@
             <div class="text-white font-semibold text-lg"><a href="">Daftar Kelas</a></div>
             <div>
                 <a href="" class="text-white hover:text-gray-200 px-4">Tambah Data</a>
+             </div>
+            </div>
+            <div>
+            <button class="btn" onclick="my_modal_1.showModal()">open modal</button>
+ <dialog id="my_modal_1" class="modal">
+  <div class="modal-box">
+  <form method="POST">
+            @csrf
+            <div class="mb-4">
+                <h3 class="text-xl font-bold mb-2">Add New Schedule</h3>
+            </div>
+            <div class="mb-4">
+                <label for="title" class="block text-gray-700  text-sm font-bold mb-2">Title:</label>
+                <input type="text" id="title" name="title" class="border border-gray-300 rounded-md py-2 px-3 w-full @error('title') is-invalid @enderror" value="{{old('title')}}">
+            </div>
+            {{--alert--}}
+            @error('title')
+            <div class="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4" role="alert">
+                <p class="font-bold">{{$message}}</p>
+            </div>
+
+            @enderror
+            {{--end alert--}}
+
+            <div class="mb-4">
+                <label for="description" class="block text-gray-700  text-sm font-bold mb-2">Description:</label>
+                <input type="text" id="description" name="description" class="border border-gray-300 rounded-md py-2 px-3 w-full @error('description') is-invalid @enderror" value="{{old('description')}}">
+            </div>
+            {{--alert--}}
+            @error('description')
+            <div class="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4" role="alert">
+                <p class="font-bold">{{$message}}</p>
+            </div>
+
+            @enderror
+            {{--end alert--}}
+
+            <div class="mb-4">
+                <label for="task" class="block text-gray-700  text-sm font-bold mb-2">Task:</label>
+                <input type="text" id="task" name="task" class="border border-gray-300 rounded-md py-2 px-3 w-full @error('task') is-invalid @enderror" value="{{old('task')}}">
+            </div>
+            {{--alert--}}
+             @error('task')
+            <div class="bg-orange-100 border-l-4 border-orange-500 text-orange-700 p-4" role="alert">
+                <p class="font-bold">{{$message}}</p>
+            </div>
+            
+            @enderror
+            {{--end alert--}}
+
+
+            <div>
+                <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" name="tambah" href="">Tambah</button>
+            </div>
+
+            <!-- <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" name="tambah" href="">Tambah</button> -->
+        </form>
+       
+        <button class="btn">Close</button>
+      </form>
+    </div>
+  </div>
+</dialog>
             </div>
         </div>
-    </nav>
-    <div>
-        <table>
-            <thead>
-                <tr>
-                    <th class="border px-4 py-2">No</th>
+</nav>
+    <div class="container mx-auto p-8">
+    <div class="overflow-x-auto">
+  <table class="table">
+    <thead>
+      <tr>
+      <th class="border px-4 py-2">No</th>
                     <th class="border px-4 py-2">Task Title</th>
                     <th class="border px-4 py-2">Deskription</th>
                     <th class="border px-4 py-2">Task</th>
@@ -30,10 +94,10 @@
                     <th class="border px-4 py-2">Status</th>
                     <th class="border px-4 py-2">Asignee</th>
                     <th class="border px-4 py-2">Category Task</th>
-                </tr>
-            </thead>
-            <tbody>
-            @forelse ($schedule as $jadwal)
+      </tr>
+    </thead>
+    <tbody>
+    @forelse ($schedule as $jadwal)
                 <tr>
                     <td class="border px-4 py-2">{{$loop -> iteration}}</td>
                     <td class="border px-4 py-2">{{$jadwal -> title}}</td>
@@ -53,10 +117,11 @@
                     </td>
                 </tr>
                 @empty
-                <h3 class="text-xl font-bold mb-5">Kuota Kelas Full</h3>
                 @endforelse
-            </tbody>
-        </table>
-    </div> 
+    </tbody>
+  </table>
+</div>
+</div> 
+
 </body>
 </html>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,13 @@
 /** @type {import('tailwindcss').Config} */
+import daisyui from "daisyui";
 export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [require('daisyui')],
-}
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {},
+    },
+    plugins: [require('daisyui')],
+};
